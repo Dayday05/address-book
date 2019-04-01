9e93ed1eac46f24aec59186e1debd67d9e01ee43
dik david <dikdavid@socgen.com>
2019-04-01 15:07:17 +0200

    WIP v2

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'app-form',
+  templateUrl: 'form.component.html',
+  styleUrls: ['form.component.css']
+})
+export class FormComponent {}
